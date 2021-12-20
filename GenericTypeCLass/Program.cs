cd21c5d85d6db5afe2f89961301e2881d78d1686
SaboteurR3 <87414376+SaboteurR3@users.noreply.github.com>
2021-12-20 20:58:21 +0400

    Add project files.

---
+++ GenericTypeCLass/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypeCLass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> passedList = new List<int> { 1, 3, 3, 8, 16, 17, 10 };
+                List<int> List = new List<int> { 10, 3, 15, 17, 8, 25 };
+                var firstList = new Set<int>(passedList);
+                var secondList = new Set<int>(List);
+                Console.Write("==========================================\nFirst set: { ");
+                foreach (var set in firstList)
+                {
+                    Console.Write(set + ", ");
+                }
+                Console.WriteLine("}");
+                Console.Write("==========================================\nSecond set: { ");
+                foreach (var set in secondList)
+                {
+                    Console.Write(set + ", ");
+                }
+                Console.WriteLine("}");
+                // AddItem method for firstlList
+                Console.WriteLine("==========================================\nAdd item: " + firstList.AddItem(5));
+                // Contains method for firstlList
+                Console.WriteLine("==========================================\nFirst list contains '8': " + firstList.Contains(8));
+                // Remove method for firstlList
+                Console.WriteLine("==========================================\nRemoves '11' from list: " + firstList.Remove(11));
+                // ISEMPTY getmethod for firstlList
+                Console.WriteLine("==========================================\nList is empty: " + firstList.ISEMPTY);
+                // COUNT getmethod for firstlList
+                Console.WriteLine("==========================================\nFirst list contains " + firstList.COUNT + " items");
+                // Union of sets
+                Set<int> result = firstList + secondList;
+                // For unique elements
+                IEnumerable<int> union = result.Distinct();
+                Console.Write("===============================================\nUnion of sets: { ");
+                foreach (int number in union)
+                {
+                    Console.Write(number + ", ");
+                }
+                Console.WriteLine("}");
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            finally
+            {
+                Console.WriteLine("===============================================\nBye <3\n");
+            }
+        }
+    }
+}
---
+++ GenericTypeCLass/Set.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypeCLass
+{
+    class Set<T> : List<T>, IEnumerable 
+    {
+        // fields
+        List<T> e = new List<T>();
+        private new int Count;
+        private bool IsEmpty;
+        // default constructor
+        public Set() { }
+        // constructor for a list 
+        public Set(List<T> e)
+        {
+            this.e = e;
+        }
+        // GetMethods
+        public int COUNT
+        {
+            get
+            {
+                Count = e.Count();
+                return Count;
+            }
+        }
+        public bool ISEMPTY
+        {
+            get
+            {
+                int quantity;
+                quantity = e.Count();
+                if (quantity == 0)
+                {
+                    IsEmpty = true;
+                    return IsEmpty;
+                }
+                else
+                {
+                    IsEmpty = false;
+                    return IsEmpty;
+                }
+            }
+        }
+        // Overloading operator '+'(Union of sets)
+        public static Set<T> operator +(Set<T> lhs, Set<T> rhs)
+        {
+            Set<T> tmp = new Set<T>();
+            foreach (T s in lhs) tmp.Add(s);
+            foreach (T s in rhs) tmp.Add(s);
+            return tmp;
+        }
+        // Add method 
+        public bool AddItem(T item)
+        {
+            if (e.Count == 0)
+            {
+                e.Add(item);
+                return true;
+            }
+            else
+                return false;
+        }
+        // Remove method 
+        public new bool Remove(T item)
+        {
+            if (e.Contains(item))
+            {
+                e.Remove(item);
+                return true;
+            }
+            else
+                return false;
+        }
+        // Contains method
+        public new bool Contains(T item)
+        {
+            if (e.Contains(item))
+                return true;
+            else
+                return false;
+        }
+        public new IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < e.Count; i++)
+            {
+                yield return e[i];
+            }
+        }
+    }
+}
